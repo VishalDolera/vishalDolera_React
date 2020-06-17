7f1c2cb3fd950a58e163d8d451dc99dd7bf51b19
Vishal Dolera <vishal.dolera@theonetechnologies.co.in>
2020-06-17 11:04:09 +0530

    Updates

---
+++ precache-manifest.a47f0dd1d1936eadfa3a05a787144016.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ba97f505ac59e339991e7b6c1b91ee0",
+    "url": "/vishalDolera_React/index.html"
+  },
+  {
+    "revision": "c8fa3621e99c813760cf",
+    "url": "/vishalDolera_React/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "14a4b8bd5c532e7f2021",
+    "url": "/vishalDolera_React/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c8fa3621e99c813760cf",
+    "url": "/vishalDolera_React/static/js/2.06052bd7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/vishalDolera_React/static/js/2.06052bd7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "14a4b8bd5c532e7f2021",
+    "url": "/vishalDolera_React/static/js/main.81a11525.chunk.js"
+  },
+  {
+    "revision": "8c2d34b39af955f89b9a",
+    "url": "/vishalDolera_React/static/js/runtime-main.fddbf73e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/vishalDolera_React/static/media/logo.5d5d9eef.svg"
+  }
+]);
